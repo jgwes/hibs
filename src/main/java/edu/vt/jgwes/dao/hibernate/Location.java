b5b84b359210e9da52401d2b280c16ff27b9d8b9
John Westerman <john.westerman@gmail.com>
2012-01-24 22:04:56 -0500

    add and configure annotated class stub Location

---
+++ src/main/java/edu/vt/jgwes/dao/hibernate/Location.java
@@ -0,0 +1,31 @@
+package edu.vt.jgwes.dao.hibernate;
+
+
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+@Entity
+@Table( name = "LOCATION" )
+public class Location {
+	private Long id;
+	
+	
+	@Id
+	@GeneratedValue(generator="increment")
+	@GenericGenerator(name="increment", strategy = "increment")
+	public Long getId() {
+	    return id;
+	}
+	
+    private void setId(Long id) {
+		this.id = id;
+    }
+	
+}
