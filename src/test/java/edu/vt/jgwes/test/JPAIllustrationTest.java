0759ef2398627b43b68466cae6e1f64553f078e7
jgwes <jgwes@vt.edu>
2012-01-30 16:11:17 -0500

    added jpa examples for postgresql

---
+++ src/test/java/edu/vt/jgwes/test/JPAIllustrationTest.java
@@ -0,0 +1,75 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package edu.vt.jgwes.test;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import edu.vt.jgwes.dao.jpa.JPAEvent;
+
+import junit.framework.TestCase;
+
+/**
+ * Illustrates basic use of Hibernate as a JPA provider.
+ *
+ * @author Steve Ebersole
+ */
+public class JPAIllustrationTest extends TestCase {
+	private EntityManagerFactory entityManagerFactory;
+
+	@Override
+	protected void setUp() throws Exception {
+		// like discussed with regards to SessionFactory, an EntityManagerFactory is set up once for an application
+		// 		IMPORTANT: notice how the name here matches the name we gave the persistence-unit in persistence.xml!
+		entityManagerFactory = Persistence.createEntityManagerFactory( "edu.vt.jgwes.jpa" );
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		entityManagerFactory.close();
+	}
+
+	public void testBasicUsage() {
+		// create a couple of events...
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		entityManager.persist( new JPAEvent( "Our very first JPA event!", new Date() ) );
+		entityManager.persist( new JPAEvent( "A follow up JPA event", new Date() ) );
+		entityManager.getTransaction().commit();
+		entityManager.close();
+
+		// now lets pull events from the database and list them
+		entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+        List<JPAEvent> result = entityManager.createQuery( "from JPAEvent", JPAEvent.class ).getResultList();
+		for ( JPAEvent event : result ) {
+			System.out.println( "JPA Event (" + event.getDate() + ") : " + event.getTitle() );
+		}
+        entityManager.getTransaction().commit();
+        entityManager.close();
+	}
+}
